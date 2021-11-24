902cfa11387838e07f8b243ea1970ee24ff359a0
Jamie Diaz <jamie.diaz@gmail.com>
2021-11-24 15:27:00 -0600

    make it better

--- website/app.py
+++ website/app.py
@@ -172,5 +172,5 @@ def wellfilesFilteredAPI(name_of_file):
     return jsonify(well_list)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-    # app.run()
+    # app.run(host='0.0.0.0')
+    app.run()
