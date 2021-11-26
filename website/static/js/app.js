0eedfe053226ce6755146ef209868d2f8df1923a
Jamie Diaz <jamie.diaz@gmail.com>
2021-11-26 16:49:59 -0600

    date time picker

---
+++ website/static/js/app.js
@@ -0,0 +1,13 @@
+function myFunction() {
+
+
+
+    var sdmDate = document.getElementById("dateSelect").value;
+    var sdmName = document.getElementById("fullName").value;
+    var sdmEmail = document.getElementById("emailAddress").value
+    console.log(sdmDate, sdmName, sdmEmail)
+
+
+    // var searchItem = document.getElementById("searchFile").value;
+    return;
+}
