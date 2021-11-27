ca43f00cc12100b98c58a363b5cb10d6154d39fe
Jamie Diaz <jamie.diaz@gmail.com>
2021-11-26 18:21:06 -0600

    API retrieval

---
+++ website/static/js/getDates.js
@@ -0,0 +1,11 @@
+function getDates() {
+
+    dataQuery = "https://savvy-booking.herokuapp.com/api/v1.0/calendar";
+
+    fetch(dataQuery).then((response) => {
+
+        console.log(response.json);
+        return response.json();
+
+    });
+}
