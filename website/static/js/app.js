ac67a388e0713b8081172738e96907daea76ebb3
Jamie Diaz <jamie.diaz@gmail.com>
2021-11-28 17:05:07 -0600

    API retrieval and saved into javascript array for disabled dates

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -6,11 +6,11 @@ function myFunction() {
     var sdmName = document.getElementById("fullName").value;
     var sdmEmail = document.getElementById("emailAddress").value
 
-
+    sdmDate = sdmDate.replace('/', '-')
 
 
     console.log(sdmDate, sdmName, sdmEmail)
-    window.location = 'https://savvy-booking.herokuapp.com/v1.0/DBentry/' + sdmDate + sdmName + sdmEmail;
+        // window.location = 'https://savvy-booking.herokuapp.com/v1.0/DBentry/' + sdmDate + sdmName + sdmEmail;
 
     // var searchItem = document.getElementById("searchFile").value;
     return;
