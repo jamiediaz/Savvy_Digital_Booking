180911d8e4107cc550dcb5ba710cc2b16a5395de
Jamie Diaz <jamie.diaz@gmail.com>
2021-11-28 15:37:09 -0600

    API retrieval and saved into javascript array for disabled dates

--- website/app.py
+++ website/app.py
@@ -114,16 +114,17 @@ def confirmed_dates_API():
 
     #Create the API using this new date range column
     conf_dates_list = []
-    conf_dates_list[0] = "{recurring: {repeat: 'weekly', weekDays: 'SA, SU'} }"
+    
     for x in df.index:
         conf_dates_dict = {}
-        #
+        
         conf_dates_dict["start"] = df['event_begins'][x]
         conf_dates_dict["end"] = df['event_ends_converted'][x]
         #conf_dates_dict['date_range'] = df['date_range'][x]
         
         conf_dates_list.append(conf_dates_dict)
-
+    conf_dates_list[-1] = "{recurring: {repeat: 'weekly', weekDays: 'SA, SU'} }"
+    
     return jsonify(conf_dates_list)
 
 @app.route("/api/v1.0/land/<name_of_file>")
