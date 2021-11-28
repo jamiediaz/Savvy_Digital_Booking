916d8a57c87add75f4531c692bc80dbbc292da86
Jamie Diaz <jamie.diaz@gmail.com>
2021-11-28 16:42:10 -0600

    API retrieval and saved into javascript array for disabled dates

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -5,11 +5,11 @@ function myFunction() {
     var sdmDate = document.getElementById("dateSelect").value;
     var sdmName = document.getElementById("fullName").value;
     var sdmEmail = document.getElementById("emailAddress").value
-    var sdmDateISO = Date(sdmDate);
+
 
     // sdmDateISO = sdmDateISO.toISOString();
 
-    console.log(sdmDateISO, sdmName, sdmEmail)
+    console.log(sdmDate, sdmName, sdmEmail)
 
 
     // var searchItem = document.getElementById("searchFile").value;
