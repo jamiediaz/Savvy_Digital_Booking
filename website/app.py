a60267dd2bc57016dd1c3e004cab71adf9e1604f
Jamie Diaz <jamie.diaz@gmail.com>
2021-11-28 22:33:35 -0600

    trying to pass input data into python through flask

--- website/app.py
+++ website/app.py
@@ -140,7 +140,7 @@ def DBentry():
     sdmStartTime = request.args.get('sdmStartTime')
     #sdmStartTime = request.form.get('sdmStartTime')
     
-    return (sdmDate, sdmStartTime)
+    return (sdmDate)
     #sdmDate = sdmDate.isoformat()
     # print(sdmDate)
     #print(sdmStartTime)
