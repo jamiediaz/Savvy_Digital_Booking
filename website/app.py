52cea76d28e1c9d7c891684aca84ac22d6d90bba
Jamie Diaz <jamie.diaz@gmail.com>
2021-11-29 17:52:51 -0600

    insert into postgres table

--- website/app.py
+++ website/app.py
@@ -167,7 +167,7 @@ def DBentry():
     
     new_SQL_row = pd.DataFrame(data)
 
-    new_SQL_row.to_sql(name='appt_requests', con=connection, if_exists='append')
+    new_SQL_row.to_sql(name='appt_requests', con=connection, if_exists='append', index=False)
     with engine.connect() as con:
         con.execute()
     
@@ -175,7 +175,7 @@ def DBentry():
     #connection.execute("INSERT INTO appt_requests(summary, description, event_begins, event_ends, attendees) VALUES (:full_name, :user_email, :start_date_time,:end_date_time,:user_email)",{"summary": full_name, "description": user_email, "event_begins": start_date_time, "event_ends": end_date_time, "attendees": user_email})
     
     
-    return (new_SQL_row)
+    return '''ok'''
     #sdmDate = sdmDate.isoformat()
     # print(sdmDate)
     #print(sdmStartTime)
