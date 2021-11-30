0e35e648fb489e0f3bd82fc5750f1d38331bbbeb
Jamie Diaz <jamie.diaz@gmail.com>
2021-11-29 18:01:47 -0600

    insert into postgres table

--- website/app.py
+++ website/app.py
@@ -166,6 +166,7 @@ def DBentry():
             'attendees':[user_email]}
     
     new_SQL_row = pd.DataFrame(data)
+    new_SQL_row = new_SQL_row.reset_index(inplace=True, drop=True)
 
     new_SQL_row.to_sql(name='appt_requests', con=connection, if_exists='append', index=False)
     with engine.connect() as con:
